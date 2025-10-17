f2446c4aeca1ae02ba1a234a71dc913bfd596986
Pedro Henrique Nascimento <phnascis@gmail.com>
2025-10-17 15:59:58 -0300

    Current codes I'm using

---
+++ Pim/Chamado.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace Pim_04.Views
+{
+    public class Chamado : INotifyPropertyChanged
+    {
+        private string? _status;
+
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public string? Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+        public string? Username { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
